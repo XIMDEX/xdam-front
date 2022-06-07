48af56470d8987773a8928f81f9c99edaba3a196
joan <jwcervera@hotmail.com>
2022-06-07 13:56:59 +0200

    ensure book unit is a number

--- src/components/forms/BookNumberOfUnitSelectorWrapper/BookNumberOfUnitSelectorWrapper.tsx
+++ src/components/forms/BookNumberOfUnitSelectorWrapper/BookNumberOfUnitSelectorWrapper.tsx
@@ -3,16 +3,20 @@ import { Button, Icon } from "semantic-ui-react";
 import { CustomBookNumberOfUnitSelector } from "../../../features/Resources/Modals/DynamicFormTemplates/CustomFields"
 import styles from "./BookNumberOfUnitSelectorWrapper.module.scss";
 
-const BookNumberOfUnitSelectorWrapper = ({ value, onChange, maxUnit, unavaliableValues }: { value: number, onChange: (children: any) => void, maxUnit?: number, unavaliableValues: number[] }) => {
+const BookNumberOfUnitSelectorWrapper = ({ value, onChange, maxUnit, unavaliableValues }: { value: number, onChange: (value: number) => void, maxUnit?: number, unavaliableValues: number[] }) => {
 
     const unsetValue = () => {
         onChange(null);
     }
 
+    const onChangeAsInt = (value: string) => {
+        onChange(parseInt(value));
+    }
+
     return (
         <div className={styles.bookNumberOfUnitSelectorWrapper}>
             <div className={styles.bookNumberOfUnitSelectorWrapper__selector}>
-                <CustomBookNumberOfUnitSelector value={value} onChange={onChange} maxOptions={maxUnit} unavaliableValues={unavaliableValues} />
+                <CustomBookNumberOfUnitSelector value={value} onChange={onChangeAsInt} maxOptions={maxUnit} unavaliableValues={unavaliableValues} />
             </div>
             
             <Button icon onClick={unsetValue} className={styles.bookNumberOfUnitSelectorWrapper__unset}>
