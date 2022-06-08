5a928127461ff4e248d5e8f23a93a9e296ea04ca
joan <jwcervera@hotmail.com>
2022-06-08 16:16:31 +0200

    create image input preview component

---
+++ src/components/forms/ImageInputPreview/ImageInputPreview.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Button } from "semantic-ui-react";
+import styles from './ImageInputPreview.module.scss';
+
+const ImageInputPreview = ({ onChange }) => {
+
+    const [previewImage, setPreviewImage] = useState(null);
+    const hiddenFileInput = React.useRef(null);
+
+    const handleClick = () => {
+        hiddenFileInput.current.click();
+    };
+
+    const previewBackground = () => {
+
+        const url = previewImage ? URL.createObjectURL(previewImage) : 'noimg.png';
+
+        return {
+            backgroundImage: `url(${url})`,
+        }
+    }
+
+    const handleFiles = (e) => {
+        const image = e.target.files[0];
+        debugger;
+        if(previewImage && !image) {
+            return;
+        }
+
+        setPreviewImage(image);
+        onChange(image);
+    }
+    
+    return (
+        <Button
+            className={styles.imageInputPreview}
+            onClick={handleClick}
+            style={previewBackground()}
+            component="label"
+            fullWidth
+            variant='outlined'
+        >
+            <input
+                accept="image/*"
+                type="file"
+                ref={hiddenFileInput}
+                onChange={(e) => handleFiles(e)}
+                name='Preview'
+                hidden
+            />
+        </Button>
+    )
+}
+
+export default ImageInputPreview;
