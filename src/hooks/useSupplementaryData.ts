15e37860081d972961ebc1d4cda6f1a701a74a08
joan <jwcervera@hotmail.com>
2022-06-21 13:26:21 +0200

    display workspace names on chips

--- src/hooks/useSupplementaryData.ts
+++ src/hooks/useSupplementaryData.ts
@@ -1,30 +1,41 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import workspacesProvider from "../api/providers/workspacesProvider";
+import { setWorkspacesData } from "../appSlice";
+import { WORKPSACES } from "../constants";
 import { Facet } from "../types/Facet";
 
-const providersMap = {
-    'workspaces': workspacesProvider
+const map = {
+    [WORKPSACES]: {
+        provider: workspacesProvider,
+        dispacher: setWorkspacesData
+    }
 }
 
 const useSupplementaryData = (facet: Facet) => {
     
     const [supplementaryData, setSupplementaryData] = useState(null);
+    const dispatch = useDispatch();
 
     useEffect(() => {
 
-        const fetchSupplementaryData = async () => {
+        const fetchSupplementaryData = async (dispatcher?) => {
             const data = await provider(Object.keys(facet.values));
 
             setSupplementaryData(data);
+
+            if(dispatcher) {
+                dispatch(dispatcher(data));
+            }
         }
-        
-        const provider = providersMap[facet.key];
+
+        const provider = map[facet.key]?.provider;
 
         if(!provider) {
             return;
         }
 
-        fetchSupplementaryData();
+        fetchSupplementaryData(map[facet.key].dispacher);
     }, []);
 
     return supplementaryData;
