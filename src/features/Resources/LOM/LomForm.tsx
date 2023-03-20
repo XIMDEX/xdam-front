c46972e668f8ecfe2d48353ea64ceed57612e1af
santiagocodes <mari.c.santiago2@gmail.com>
2023-03-20 17:21:56 +0100

    ADD: export lomform

--- src/features/Resources/LOM/LomForm.tsx
+++ src/features/Resources/LOM/LomForm.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import Lom from './Lom'
 
-function LomForm({data, standard}) {
+export function LomForm({data, standard}) {
   return (
     <Grid container>
       <Grid item={true} sm={12} style={{width: '100%'}}>
