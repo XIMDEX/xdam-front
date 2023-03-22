1cccb1fb564f6c41dedc1f4e16517941295a4443
santiagocodes <mari.c.santiago2@gmail.com>
2023-03-22 11:24:27 +0100

    ADJUST: limited lom use style

--- src/features/Resources/LOM/LomForm.tsx
+++ src/features/Resources/LOM/LomForm.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import Lom from './Lom'
 
-export function LomForm({data, standard}) {
+export function LomForm({data, standard, limitedUse=false }) {
   return (
     <Grid container>
-      <Grid item={true} sm={12} style={{width: '100%'}}>
+      {limitedUse && 
+        <h1 style={{padding: '0.5em', marginTop: '1em', textAlign: 'center', backgroundColor: '#fff', width: 'calc(100% - 2rem)'}}>
+              Limited use (LOM only)
+        </h1>
+      }
+      <Grid item={true} sm={12} style={{width: '100%', ...(limitedUse && {marginBottom: '1em'} )}}>
         <Lom resourceData={data} standard={standard}/>
       </Grid>
     </Grid>
