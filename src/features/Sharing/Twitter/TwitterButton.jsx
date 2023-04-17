a98178959fb5afae7180784f4f699b4eabbe4ade
Giovanni <giovanni.alberto@hotmail.com>
2023-04-17 16:00:33 +0100

    fix: url for tweet

--- src/features/Sharing/Twitter/TwitterButton.jsx
+++ src/features/Sharing/Twitter/TwitterButton.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import { API_BASE_URL  } from '../../../constants';
 
 import './TwitterButton.css';
 
 const TwitterButton = ({ text, url }) => {
   const baseUrl = 'https://twitter.com/intent/tweet?';
   const tweetText = `text=${encodeURIComponent(text)}`;
-  const tweetUrl = url ? `&url=${encodeURIComponent(window.location.origin)}/image?image_url=${encodeURIComponent(url)}` : '';
+  const tweetUrl = url ? `&url=${API_BASE_URL}/resource/render/${url}/html` : '';
 
   const shareUrl = `${baseUrl}${tweetText}${tweetUrl}`;
 
