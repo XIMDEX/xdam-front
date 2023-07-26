c45507ec45f551763d6689c8b0ae97764955b754
juandz13 <jdiaz@ximdex.com>
2023-07-26 12:14:57 +0200

    showing ai data

--- src/features/Resources/Tabs/AiData.tsx
+++ src/features/Resources/Tabs/AiData.tsx
@@ -1,33 +1,46 @@
-import React, { useEffect } from 'react'
-import MainService from '../../../api/service';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectFormData, setAiData, setFormData } from '../../../appSlice';
+import React, { useEffect } from "react";
+import MainService from "../../../api/service";
+import { useDispatch, useSelector } from "react-redux";
+import { selectFormData, setAiData, setFormData } from "../../../appSlice";
 
 const AiData = (props) => {
-    const dispatch = useDispatch();
-    let storeFormData = useSelector(selectFormData);
-    const getResourceData = async () => {
-        const uuid = props.id;
-        let res = await MainService().getResourceJson(uuid);
-        const result = await {...storeFormData,...res} 
-        await dispatch(setFormData(result));
-    }
-    useEffect(() => {
-      if (!storeFormData.imageCaptionAi) {
-        getResourceData();
-      }
-    })
-    
-    return (
-      <div>
-        <p>Semantic data:</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt suscipit dignissimos ea labore ipsa illum quas temporibus cupiditate error laudantium, impedit perspiciatis modi. Rerum, cum fugiat? Optio repudiandae dignissimos vero.</p>
-        {storeFormData.imageCaptionAi && <div><p>Image Data:</p>
-        <p>{storeFormData.imageCaptionAi}</p></div>
-        }
-        
-      </div>
-    );
+  const dispatch = useDispatch();
+  let storeFormData = useSelector(selectFormData);
+  const getResourceData = async () => {
+    const uuid = props.id;
+    let res = await MainService().getResourceJson(uuid);
+    const result = await { ...storeFormData, aiData: { ...res } };
+    await dispatch(setFormData(result));
   };
+  useEffect(() => {
+    if (!storeFormData.aiData) {
+      getResourceData();
+    }
+  });
+  const flexLine = {"display":"flex","padding":"0.5rem","flexDirection":"row","flexWrap":"wrap","gap":"1rem"};
+  
+  return (
+    <div>
+      {storeFormData.aiData && (
+        <div>
+          {storeFormData.aiData.xtags && (
+            <div>
+              <h3 style={{ color:"#43a1a2" }}>XTags(Unlinked)</h3>
+              <div  style={flexLine}>{storeFormData.aiData.xtags.map(xtag => <p>{xtag.name}</p>)}</div>
+              <h3 style={{ color:"#43a1a2" }}>XTags</h3>
+              <div  style={flexLine}>{storeFormData.aiData.xtags_interlinked.map(xtag => <p>{xtag.name}</p>)}</div>
+            </div>
+          )}
+          {storeFormData.aiData.imageCaptionAi && (
+            <div>
+              <h3 style={{ color:"#43a1a2" }}>Image</h3>
+              <div  style={flexLine}>{storeFormData.aiData.imageCaptionAi}</div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-  export default AiData;
+export default AiData;
