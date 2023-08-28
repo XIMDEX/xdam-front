5ed960ca049cab5a63b21f32e5c3aac3c8195017
juandz13 <jdiaz@ximdex.com>
2023-08-28 09:41:38 +0200

    bugfix tabs

--- src/features/Resources/Modals/DynamicFormTemplates/DynamicFormTabs.tsx
+++ src/features/Resources/Modals/DynamicFormTemplates/DynamicFormTabs.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import { Tab } from "semantic-ui-react";
 import { VALIDS_LOM } from "../../../../constants";
 import LomForm from "../../LOM/LomForm";
-import { useState } from "react";
 import { Grid } from "@material-ui/core";
 import AiData from '../../Tabs/AiData';
 import store from '../../../../app/store';
@@ -12,37 +11,29 @@ const DynamicFormTabs = (props) => {
     const metaData = { menuItem: 'Main Data', render: () => <Tab.Pane > {<props.mainData/>}</Tab.Pane> };
     const lomsData = !props.showLom ? [] : VALIDS_LOM.map(typeLom => ({menuItem: typeLom.name, render: () => (<Tab.Pane><LomForm data={props.dataForUpdate} standard={typeLom.key}/></Tab.Pane>)}))
     const pane = [metaData];
-   // const panes = [metaData];
-    const [panes, setPanes] = useState([metaData, ...lomsData])
     const storeTags = store.getState().app.formData; 
-    useEffect(() => {
- 
-
-        if (typeof storeTags.description !== 'undefined' || storeTags.description !== null ){
-            
-          const entities = storeTags.description.entities_linked ?? "";
-          const uuids = [];
-          const uuidsTabs = [];
-          entities.forEach(element => {
-            if (uuids.indexOf(element.uuid) === -1) {
-              uuids.push(element.uuid);
-            }
-          });
-         uuids.forEach(element => {
-          uuidsTabs.push({ menuItem: element, render: () => <Tab.Pane > <AiData  uuid={element} /></Tab.Pane> });
-         });
-      
-        //  console.log(store.getState().app.formData.description.entities_linked);
-      
-          setPanes([...panes,...uuidsTabs])
-          console.log(panes)
+    const panes = () => {
+        if (storeTags !== null && typeof storeTags !== 'undefined'){
+            const entities = storeTags.description.entities_linked ?? "";
+            const uuids = [];
+            const uuidsTabs = [];
+            entities.forEach(element => {
+              if (uuids.indexOf(element.uuid) === -1) {
+                uuids.push(element.uuid);
+              }
+            });
+           uuids.forEach(element => {
+            uuidsTabs.push({ menuItem: element, render: () => <Tab.Pane > <AiData  uuid={element} /> </Tab.Pane> });
+           });
+            return [metaData, ...uuidsTabs];
         }
       
-      }, [])
+    }
+
     return (
-         
+     
         <Grid item sm={12} id='form-content'>
-          {props.dataForUpdate && props.action === 'edit' ? (<Tab panes={panes} />) : (<Tab panes={pane} />)}
+                {storeTags &&  props.dataForUpdate && props.action === 'edit' && panes ? (<Tab panes={panes()} />) : (<Tab panes={pane} />)}
         </Grid>
            
           
