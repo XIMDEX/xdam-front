1dbf67ace959554daae08356a7b4d6e4003f6c36
juandz13 <jdiaz@ximdex.com>
2024-02-12 12:46:08 +0100

    xtags update

--- src/features/Resources/Modals/DynamicForm/MediaData.tsx
+++ src/features/Resources/Modals/DynamicForm/MediaData.tsx
@@ -5,6 +5,15 @@ import { XTag } from '@ximdex/xui-react/material';
 const MediaData = (props) => {
     const src = props.url
     console.log(props.url)
+    const tag = {
+        id: "demo1",
+        label: "Demo Tag 1",
+        vocabulary: "VOCABULARY",
+        description: "DESCRIPTION",
+        type: "TYPE",
+        link: "LINK"
+      }
+
     return (
         <div style={{"display":"flex","padding":"1rem","justifyContent":"center","flexDirection":"column","margin":"auto","borderWidth":"8px",}}>
             <img src={MainService().render(src)} alt="Not Found" style={{"maxHeight":"250px","margin":"auto","objectFit":"contain"}} />
@@ -18,11 +27,44 @@ const MediaData = (props) => {
             </div>
             <div style={{"borderTop":"2px solid #eaeaea","padding":"4px","display":"flex","justifyContent":"center","flexDirection":"column","gap":"0.5rem"}}>
                 <label style={{"fontWeight":"bold"}}>XTAGS</label>
+                <div style={{"display":"flex","flexWrap":"wrap"}}>
                 <XTag
-                    name='testing tag'
-                    status='correct'
-                    onClickRemove={() => console.log("remove")}
-                />
+                    key={'v2_' + 1 + '_' + 1}
+                    tag={tag}
+                    customizable={{custom: {
+                        color: 'red'
+                    }}}
+                 />
+                  <XTag
+                    key={'v2_' + 11 + '_' + 1}
+                    tag={tag}
+                    customizable={{custom: {
+                        color: 'red'
+                    }}}
+                 />
+                  <XTag
+                    key={'v2_' + 12 + '_' + 1}
+                    tag={tag}
+                    customizable={{custom: {
+                        color: 'red'
+                    }}}
+                 />
+                  <XTag
+                    key={'v2_' + 13 + '_' + 1}
+                    tag={tag}
+                    customizable={{custom: {
+                        color: 'red'
+                    }}}
+                 />
+                  <XTag
+                    key={'v2_' + 14 + '_' + 1}
+                    tag={tag}
+                    customizable={{custom: {
+                        color: 'red'
+                    }}}
+                 />
+                </div>
+                
             </div>
         </div>
     )
