4686725d6cc6e340791adfb64879fcb25652476a
juandz13 <jdiaz@ximdex.com>
2024-02-13 09:20:42 +0100

    update

--- src/features/Resources/Modals/DynamicForm/mediaData/MediaBox.tsx
+++ src/features/Resources/Modals/DynamicForm/mediaData/MediaBox.tsx
@@ -3,10 +3,27 @@ import React from 'react';
 
 const MediaBox = ({src}) => {
     const fileName      = MainService().render(src);
-    //const fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+    const checkTypeFile = () => {
+        if(src.includes("@@@dam:@image")){
+            return  "image"
+        }else if(src.includes("@@@dam:@video")) {
+            return "video"
+        }else if(src.includes("@@@dam:@audio")) {
+            return "audio"
+        }else if(src.includes("@@@dam:@document")) {
+            return "document"
+        }
+    }
     console.log(fileName)
     return (
-        <img src={fileName} alt="Not Found" style={{"maxHeight":"250px","margin":"auto","objectFit":"contain"}} />
+        <>
+        {checkTypeFile() === "video" && (
+          <video src={fileName} controls style={{ maxHeight: "250px", margin: "auto", objectFit: "contain" }} />
+        )}
+        {checkTypeFile() !== "video" && (
+          <img src={fileName} alt="Not Found" style={{ maxHeight: "250px", margin: "auto", objectFit: "contain" }} />
+        )}
+      </>
     )
     
 }
