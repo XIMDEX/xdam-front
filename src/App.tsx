5716feef39ce5886f08cf128f8017f0a3629ab83
Fran Enriquez <fran.enriquez.dev@gmail.com>
2024-02-16 08:38:19 +0100

    FIX: handle hash or uuid on previewResourece

--- src/App.tsx
+++ src/App.tsx
@@ -184,7 +184,9 @@ function App() {
     const urlParts = location.pathname.split('/');
     const resourceId = urlParts[2];
     const params = new URLSearchParams(location.search);
-    const isCDN = params.get('cdn') === 'true';
+    const regexHashIsUUID = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[0-4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$');
+
+    const isCDN = !regexHashIsUUID.test(resourceId) || params.get('cdn') === 'true';
 
     return (
           <Container maxWidth='xl' disableGutters>
