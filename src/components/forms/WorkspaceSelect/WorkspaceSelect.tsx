b1d32d6f6ee64295c10879385a545f9e7525f01f
juandz13 <jdiaz@ximdex.com>
2024-03-12 08:31:30 +0100

    BUGFIX

--- src/components/forms/WorkspaceSelect/WorkspaceSelect.tsx
+++ src/components/forms/WorkspaceSelect/WorkspaceSelect.tsx
@@ -4,8 +4,8 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { faPlusCircle, faCheckCircle, faXmarkCircle} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { selectWorkspacesData } from "../../../appSlice";
-import { useSelector } from 'react-redux';
+import { selectResources, selectWorkspacesData, setResources, setWorkspacesData } from "../../../appSlice";
+import { useDispatch, useSelector } from 'react-redux';
 import MainService from '../../../api/service';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
@@ -47,6 +47,9 @@ interface IWkoptions {
 export default function WorkspaceSelect({resourceData, dataForUpdate}) {
     const classes = useStyles();
     const workspaces = useSelector(selectWorkspacesData);
+    const resources  = useSelector(selectResources)
+    const dispatch = useDispatch()
+    const workspacesList = resources.find(resource => resource.id === dataForUpdate.id).workspaces
     const [addWorkspace,setAddWorkspace] = useState(false);
     const [newWorkspaceValue, setNewWorkspaceValue] = useState("");
     const workspaceDefault = [{
@@ -64,67 +67,27 @@ export default function WorkspaceSelect({resourceData, dataForUpdate}) {
         })
         return workspaceArray
     })
-
-
-    const handleWorkspaceSelect = async (e: React.ChangeEvent<EventTarget>, values: Array<IWkoptions>) => {
-        let newWorkspaces = []
-        values.forEach(option => {
-            let obj = {
-                name: option.label,
-                id: option.value,
-            }
-            newWorkspaces.push(obj)
-        });
-        let data = new FormData();
-        data.append('workspaces', JSON.stringify(newWorkspaces))
-
-        await MainService().setWorkspaceResource(resourceData.id, data)
-    };
-
-    const handleAddWorkspace = async () =>{
-        let values = workspacesOptions.filter((wkOption: IWkoptions) => dataForUpdate.workspaces.includes(String(wkOption.value)))
-        let newWorkspaces = []
-        values.forEach(option => {
-            let obj = {
-                name: option.label,
-                id: option.value
-            }
-            newWorkspaces.push(obj)
-        });
-        newWorkspaces.push({
-            name: newWorkspaceValue,
-            id: -1
-        })
-        let data = new FormData();
-        data.append('workspaces', JSON.stringify(newWorkspaces))
-        await MainService().setWorkspaceResource(resourceData.id, data)
-        setAddWorkspace(false)
+    const setResource = (workspaces) => {
+        let resourceIndex = resources.findIndex(obj => obj.id === resourceData.id);
+        if (resourceIndex !== -1) {
+            let updatedResource = {
+                ...resources[resourceIndex],
+                workspaces: workspaces.map(workspace => String(workspace))
+            };
+            let updatedResources = [...resources];
+            updatedResources[resourceIndex] = updatedResource;
+            dispatch(setResources(updatedResources));
+        }
     }
-
-    return ( <div style={{display: 'flex'}}>
-    {addWorkspace
-    ?
-        <input
-            autoFocus={true}
-            name='New Workspace'
-            type="text"
-            placeholder="New workspace"
-            defaultValue={newWorkspaceValue}
-            onChange={(event) => {
-                setNewWorkspaceValue(event.target.value)}
-            }
-            className={classes.addWorkspaceInput}
-        />
-    :
-        (dataForUpdate !== null
-        ?
+    const MyAutocomplete = () => {
+        return (
             <Autocomplete
             className={classes.workspaceSelect}
             multiple
             id="tags-standard"
             options={workspacesOptions}
             onChange={(e,values) => handleWorkspaceSelect (e, values)}
-            defaultValue={workspacesOptions.filter((wkOption: IWkoptions) => dataForUpdate.workspaces.includes(String(wkOption.value)))}
+            defaultValue={workspacesOptions.filter((wkOption: IWkoptions) => workspacesList.includes(String(wkOption.value)))}
             size="small"
             clearIcon={false}
             renderInput={(params) => (
@@ -146,6 +109,85 @@ export default function WorkspaceSelect({resourceData, dataForUpdate}) {
                 ))
             }
             />
+        );
+    };
+    
+
+    const handleWorkspaceSelect = async (e: React.ChangeEvent<EventTarget>, values: Array<IWkoptions>) => {
+        let newWorkspaces = []
+        values.forEach(option => {
+            let obj = {
+                name: option.label,
+                id: option.value,
+            }
+            newWorkspaces.push(obj)
+        });
+        let data = new FormData();
+        data.append('workspaces', JSON.stringify(newWorkspaces))
+        setResource(newWorkspaces.map(workspace => workspace.id))
+        await MainService().setWorkspaceResource(resourceData.id, data)
+    };
+
+    const handleAddWorkspace = async () =>{
+        let values = workspacesOptions.filter((wkOption: IWkoptions) => dataForUpdate.workspaces.includes(String(wkOption.value)))
+        let newWorkspaces = []
+        values.forEach(option => {
+            let obj = {
+                name: option.label,
+                id: option.value
+            }
+            newWorkspaces.push(obj)
+        });
+        newWorkspaces.push({
+            name: newWorkspaceValue,
+            id: -1
+        })
+        let data = new FormData();
+        data.append('workspaces', JSON.stringify(newWorkspaces))
+        
+        const res = await MainService().setWorkspaceResource(resourceData.id, data)
+        setResource(res.resource.workspaces.map(workspace => String(workspace.id)))
+        let workspaceArray = [...workspaceDefault]
+        res.resource.workspaces.map(workspace => {
+            let obj = {
+                label: workspace.name,
+                value: Number(workspace.id)
+            }
+            workspaceArray.push(obj)
+        })
+        const newValue = res.resource.workspaces[res.resource.workspaces.length - 1]
+        const newWorkspace = {
+            count: 1,
+            selected: false,
+            radio: false,
+            name: newValue.name,
+            canBeEdit: true,
+            id: newValue.id
+        }
+        const workspacesArray = {...workspaces, [newValue.id]: newWorkspace}
+        setWorkspacesOptions(workspaceArray)
+        dispatch(setWorkspacesData(workspacesArray))
+        setAddWorkspace(false)
+    }
+
+    return ( <div style={{display: 'flex'}}>
+    {addWorkspace
+    ?
+        <input
+            autoFocus={true}
+            name='New Workspace'
+            type="text"
+            placeholder="New workspace"
+            defaultValue={newWorkspaceValue}
+            onChange={(event) => {
+                setNewWorkspaceValue(event.target.value)}
+            }
+            className={classes.addWorkspaceInput}
+        />
+    :
+        (dataForUpdate !== null
+        ?
+            <MyAutocomplete/>
         :
             (null)
         )
