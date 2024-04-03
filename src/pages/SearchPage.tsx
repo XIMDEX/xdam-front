0dac290da00d7ff86823039af71854e9b9c6b45e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-04-03 09:23:05 +0200

    Update SearchPage.tsx

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,20 +2,17 @@ import React from "react";
 import { Header } from "../features/Layout/Header/Header";
 import Container from "@material-ui/core/Container";
 import { Grid } from "@material-ui/core";
-import XthemeProvider from "../providers/XthemeProvider";
 import Search from "../features/Search/Search";
 
-function SearchPage({user}) {
+function SearchPage() {
     return (
         <Container maxWidth="xl" disableGutters>
-            <XthemeProvider>
-                <Grid container>
-                    <Grid item sm={12} className="main-header">
-                        <Header _user={user} />
-                    </Grid>
+            <Grid container>
+                <Grid item sm={12} className="main-header">
+                    <Header/>
                 </Grid>
-                <Search />
-            </XthemeProvider>
+            </Grid>
+            <Search/>
         </Container>
     );
 }
